671783554bc567f13182ce16a20ae5914fc87d31
stephen-otieno <stephenotieno8781@gmail.com>
2025-01-20 16:29:50 +0300

    media

--- FiloArts/settings.py
+++ FiloArts/settings.py
@@ -133,8 +133,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Where static files will b
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/clients/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
