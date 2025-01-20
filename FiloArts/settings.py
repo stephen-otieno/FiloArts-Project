0006bb7255dbe5212a002bf070ba72c509afdbe3
stephen-otieno <stephenotieno8781@gmail.com>
2025-01-20 13:22:31 +0300

    added whitenoise package

--- FiloArts/settings.py
+++ FiloArts/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,9 +134,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/clients/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
